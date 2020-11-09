a08227068a996c5726fcefa2b6df8d39ad3fdb00
renee212020 <71196944+renee212020@users.noreply.github.com>
2020-11-08 22:26:52 -0500

    ex3

--- lab8ex3.js
+++ lab8ex3.js
@@ -3,6 +3,8 @@ document.write("Output Zero Here");
 
 //This will output when a button is pressed
 function therest(){
+
+  document.ElementById("output1").innerHTML="Output One Here";
+  document.ElementById("output2").innerHTML="Output Two Here";
 	document.ElementById("output3").innerHTML="Output Three Here";
 }
-
