43dcc0ff3fd22293bf289c644f306557b48fc960
renee212020 <71196944+renee212020@users.noreply.github.com>
2020-11-08 22:16:41 -0500

    Update lab8ex2.js

--- lab8ex2.js
+++ lab8ex2.js
@@ -11,7 +11,7 @@ function mix(){
 	var third = "#AB00FF";
 
 	document.getElementById("blue").style.color=first;
-	document.getElementBy("green").style.colour=second;
+	document.getElementById("green").style.colour=second;
 	document.getElementById("red").style.color=third;
 
 }
